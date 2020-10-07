1fcb593342eed2b1ac59efadd3a10ce3488b9572
katiec <katiecampbell21@outlook.com>
2020-10-07 14:29:45 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,20 @@
+const { urlencoded } = require('express');
+// require express
+const express = require('express');
+
+// initialize the app and create port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(urlencoded({ extended:true }));
+
+app.use(express.static("public"));
+
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+
+
+app.listen(PORT, ()=> console.log('Listening on: http://localhost:'+ PORT))
